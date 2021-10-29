ecf2c12f4b0fc2ab0359a10693916cbb4692d424
liamsefton <88951421+liamsefton@users.noreply.github.com>
2021-10-29 12:33:21 -0700

    Add files via upload
    bit of code to calculate the discrete probability distribution of an n-input gate

---
+++ DAGPropagation/AnyInputGateAttempt1.py
@@ -0,0 +1,39 @@
+original_inputs = [.5, .4, .6, .8]
+
+processed_inputs = [[.5, .5], [.6, .4], [.4, .6], [.2, .8]] #[[x1', x1], [x2', x2], ..., [xn', xn]]
+
+b_list = [0, 0, 0, 0]
+
+def addOneBinary(b_list):
+    carry = False
+    for i in range(len(b_list)):
+        if carry:
+            if b_list[len(b_list) - 1 - i] == 0:
+                b_list[len(b_list) - 1 - i] = 1
+                carry = False
+                break
+            else:
+                b_list[len(b_list) - 1 - i] = 0
+        else:
+            if b_list[len(b_list) - 1 - i] == 1:
+                carry = True
+                b_list[len(b_list) - 1 - i] = 0
+            else:
+                b_list[len(b_list) - 1 - i] = 1
+                break
+
+def getStateProbabilities(p_inputs, b_list):
+    probabilities = []
+    for i in range(2**len(b_list)):
+        probability = 1
+        for j in range(len(p_inputs)):
+            probability = probability * p_inputs[j][b_list[j]]
+        probabilities.append(probability)
+        addOneBinary(b_list)
+    return probabilities
+
+print(getStateProbabilities(processed_inputs, b_list))
+
+
+
+
