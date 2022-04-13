f68693e963c6b99055e77f366e362dd2325682b6
liamsefton <88951421+liamsefton@users.noreply.github.com>
2022-04-13 16:36:30 -0700

    Add files via upload

---
+++ liam_circuit_prop_proto.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('../../../pythonmain/install')
+import Verific
+import networkx as nx
+from liberty.parser import parse_liberty
+
+#This function is from the Verific example and is used for elaborating the netlist-liberty pair
+def Accumulate(nl,done):
+    if not nl:
+        return
+    if done.GetItem(nl):
+        return
+    inst_iter = Verific.InstanceMapIter(nl.GetInsts())
+    inst = inst_iter.First()
+    while inst:
+        Accumulate(inst.View(),done)
+        inst = inst_iter.Next()
+    done.Insert(nl)
+
+#cell class used for propagation
+class Cell:
+    def __init__(self, cell_type, inputs, name):
+        self.cell_type = cell_type
+        self.inputs = inputs
+        self.name = name
+        self.output = -1
+    
+    def update_inputs(self, cell_dict):
+        for key in self.inputs.keys():
+            if isinstance(self.inputs[key], float):
+                pass
+            else:
+                self.inputs[key] = cell_dict[self.inputs[key]].output
+
+
+    def get_leakage(self, lib_dict): #very simple placeholder function
+        ret = 0
+        if "Q" in lib_dict[self.cell_type][0]:
+            #count = 0
+            #for key in lib_dict[self.cell_type][1].keys():
+            #    ret += lib_dict[self.cell_type][1][key]
+            #    count += 1
+            #return ret / count
+            return 20
+        else:
+            when_dict = lib_dict[self.cell_type][1]
+            for when in when_dict:
+                mult = 1
+                when_split = when.strip("()").split("&")
+                for chunk in when_split:
+                    if chunk[0] == "!":
+                        mult *= 1 - self.inputs[chunk[1]]
+                    else:
+                        mult *= self.inputs[chunk[0]]
+                ret += mult * when_dict[when]
+            return ret
+
+    def __str__(self):
+        return "{" + self.cell_type + ", " + str(self.inputs) + ", " + self.name + "}"
+
+
+#LIB DICT: {CELL_TYPE_NAME : (CELL_FUNCTION, {WHEN_VALUE : LEAKAGE VALUE})} 
+library = parse_liberty(open("liberty_counter4.lib").read())
+cell_groups = library.get_groups('cell')
+lib_dict = {}
+for cell_group in cell_groups:
+    leakage_groups = cell_group.get_groups('leakage_power')
+    leakage_dict = {}
+    for leakage_group in leakage_groups:
+        leakage_dict[str(leakage_group.get("when")).strip("\"")] = float(leakage_group.get("value"))
+    pin_groups = cell_group.get_groups('pin')
+    function = "ERROR: FUNCTION NOT FOUND"
+    for pin_group in pin_groups:
+        if pin_group.get('function') != None:
+            function = str(pin_group.get('function')).strip("\"")
+    lib_dict[str(cell_group.args[0])] = (function, leakage_dict)
+
+print(lib_dict)
+
+
+synlib_reader = Verific.synlib_file()
+synlib_reader.Read("liberty_counter4.lib", "work")
+
+nl_reader = Verific.veri_nl_file()
+nl_reader.Read("netlist_counter.v","work")
+
+top = Verific.Netlist_PresentDesign()
+
+print ("INFO: top level design is %s(%s)" % (top.Owner().Name(), top.Name()))
+set = Verific.Set()
+Accumulate(top,set)
+
+set_iter = Verific.NetlistSetIter(set)
+
+cell_dict = {}
+connections_list = []
+netlist = set_iter.First()
+
+while netlist:
+    if netlist.Owner().Name() == "top": #right now it only analyzes the module named top
+        instances_map = Verific.InstanceMapIter(netlist.GetInsts())
+        instance = instances_map.First()
+        while instance:
+            print(" INSTANCE NAME: " + instance.Name() + " Cell Type: " + instance.View().Owner().Name())
+            atts_map = Verific.AttMapIter(instance.View().GetAtts())
+            port_refs_map = Verific.PortRefMapIter(instance.GetPortrefs())
+            port_ref = port_refs_map.First()
+            port_dict = {}
+            while port_ref:
+                if port_ref.GetPort().IsInput():
+                    try:
+                        port_dict[port_ref.GetPort().Name()] = port_ref.GetNet().Driver().Name()
+                        if "Q" in lib_dict[port_ref.GetNet().Driver().View().Owner().Name()][0]:
+                            port_dict[port_ref.GetPort().Name()] = .5 #if the cell driving the current input port is sequential, set input to be .5
+                        if "Q" not in lib_dict[instance.View().Owner().Name()][0]:
+                        	connections_list.append((port_ref.GetNet().Driver().Name(), instance.Name())) #if current instance is not sequential, add an edge to the connection list with the current instance as the cell taking input
+                    except:
+                        port_dict[port_ref.GetPort().Name()] = .5 #default value
+
+                    print("        Input port: " + port_ref.GetPort().Name())
+                else:
+                    print("        Output Port: " + port_ref.GetPort().Name())
+
+                port_ref = port_refs_map.Next()
+            print(port_dict)
+            cell_dict[instance.Name()] = Cell(str(instance.View().Owner().Name()), port_dict, instance.Name()) #add instance to cell dict for referencing
+
+            instance = instances_map.Next()
+
+
+    netlist = set_iter.Next()
+print("********CONNECTIONS LIST***********")
+print(connections_list)
+
+
+print("********CELL DICT************")
+for key in cell_dict.keys():
+    print("KEY: " + key + " -> VALUE: " + str(cell_dict[key]) + " and has function " + lib_dict[cell_dict[key].cell_type][0])
+
+design_dag = nx.DiGraph(connections_list)
+
+result_list = list(nx.topological_sort(design_dag))
+
+print("**********WHEN VALUES*********")
+for cell in lib_dict.keys():
+    print("CELL TYPE: " + cell)
+    for when in lib_dict[cell][1].keys():
+        print("    " + when)
+
+print("**********TOPOLOGICALLY SORTED DAG************")
+print(result_list)
+
+print("**************PROPAGATION****************")
+total_leakage = 0
+import func_generator
+func_generator.test(lib_dict)
+from output_func import func_dict
+for cell_name in result_list:
+    cell = cell_dict[cell_name]
+    cell.update_inputs(cell_dict)
+    cell.output =  func_dict[cell.cell_type](cell.inputs)
+    print(cell_name + " HAS OUTPUT " + str(cell.output))
+    cell_leakage = cell.get_leakage(lib_dict)
+    print(cell_name + " HAS LEAKAGE " + str(cell_leakage))
+    total_leakage += cell_leakage
+
+print("TOTAL LEAKGE = " + str(total_leakage))
+
+sys.exit(0)
